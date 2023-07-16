8ce382267b6cbeace9a3ccf2c3400a271436132e
alimran02 <imran-hossain@vicdigit-technologies.com>
2023-07-16 10:46:16 +0600

    frontend design

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import { decrement, increment, incrementByAmount } from "./redux/features/counter/counterSlice";
-import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import { Toaster } from './components/ui/Toaster';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
-
-  const {count} = useAppSelector((state) => state.counter);
-  const dispatch = useAppDispatch();
-
- 
   return (
-    <div className="text-center mt-10">
-      <h2 className="p-10 text-red-400 text-2xl">Counter</h2>
-    <button onClick={()=> dispatch(increment())} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
- 
-          increment
-     </button>
-     <div>{count}</div>
-     <button onClick={()=> dispatch(decrement())} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Decrement
-      </button>
-     <button onClick={()=> dispatch(incrementByAmount(5))} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        ByAmount
-      </button>
+    <div>
+      <Toaster />
+      <MainLayout />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
